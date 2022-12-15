4454dadb958fd7a500cd1e37620f092edbc2ff69
nathoandrews1 <nathoandrews@gmail.com>
2022-12-15 05:27:46 +0000

    Added File Encryption, instead of text encryption

--- js/encrypt.js
+++ js/encrypt.js
@@ -4,7 +4,7 @@ var decrypted;
 //On Click function to for encrypt button
 document.getElementById("encryptBtn").onclick = function() {
   if(checkInputFields()) {
-    encrypt();
+    encryptFile();
   } else {
     alert("Please fill in all fields.");
   }
@@ -14,7 +14,7 @@ document.getElementById("encryptBtn").onclick = function() {
 if(document.getElementById("decryptBtn")) {
   document.getElementById("decryptBtn").onclick = function () {
     if(checkInputFields()) {
-      decrypt();
+      decryptFile();
     } else {
       alert("Please fill in all fields.");
     }
@@ -57,7 +57,16 @@ function decrypt() {
   }
 }
 
-//function to encrypt a file with cryptojs
+//Download file function
+function downloadFile() {
+  var file = document.getElementById("file").files[0];
+  var link = document.getElementById("download");
+  link.href = window.URL.createObjectURL(file);
+  link.download = file.name;
+  link.click();
+}
+
+//Create function to encrypt a file with cryptojs and download it
 function encryptFile() {
   var file = document.getElementById("file").files[0];
   var key = document.getElementById("key").value;
@@ -67,26 +76,47 @@ function encryptFile() {
     var encrypted = CryptoJS.AES.encrypt(fileData, key);
     var blob = new Blob([encrypted], {type: "text/plain"});
     var link = document.getElementById("download");
-    link.setAttribute("href", window.URL.createObjectURL(blob));
-    link.href = window.URL.createObjectURL(blob);
-    link.download = file.name + ".encrypted";
+    link.setAttribute("href", "data:application/octet-stream," + encrypted);
+    link.setAttribute("download", file.name + ".encrypted");
+    //link.href = window.URL.createObjectURL(encrypted);
     link.click();
   }
+  reader.readAsDataURL(file);
 }
 
-//function to decrypt a file with cryptojs
+//Create function to decrypt a file with cryptojs and download it
 function decryptFile() {
   var file = document.getElementById("file").files[0];
   var key = document.getElementById("key").value;
   var reader = new FileReader();
   reader.onload = function (e) {
     var fileData = e.target.result;
-    var decrypted = CryptoJS.AES.decrypt(fileData, key);
+    var decrypted = CryptoJS.AES.decrypt(fileData, key).toString(CryptoJS.enc.Latin1);
     var blob = new Blob([decrypted], {type: "text/plain"});
     var link = document.getElementById("download");
-    link.href = window.URL.createObjectURL(blob);
-    link.download = file.name;
+    //link.href = window.URL.createObjectURL(decrypted);
+
+    //remove .encrypted from file name
+    var fileName = file.name;
+    fileName = fileName.replace(".encrypted", "");
+    link.setAttribute("download", fileName);
+    link.setAttribute("href", decrypted);
     link.click();
+  }
+  reader.readAsText(file);
+}
+
+//Function to check if file is encrypted
+function checkFile() {
+  var file = document.getElementById("file").files[0];
+  var reader = new FileReader();
+  reader.onload = function (e) {
+    var fileData = e.target.result;
+    var decrypted = CryptoJS.AES.decrypt(fileData, "test").toString(CryptoJS.enc.Latin1);
+    if (decrypted == "") {
+      document.getElementById("file").value = "";
+      alert("File is not encrypted.");
+    }
   }
 }
 
