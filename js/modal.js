48e32d2e78e1f598724aad79a6a653e8d9943ef6
nathoandrews1 <nathoandrews@gmail.com>
2023-02-01 04:26:41 +0000

    Cleaned UI Elements and Help popup

--- js/modal.js
+++ js/modal.js
@@ -1,7 +1,9 @@
-var msgEncrypHelp = "Type a message into the message box and enter a password and click encrypt, the generated encrypted message will be displayed in the encrypted message box.";
-var msgDecryptHelp = "Paste an encrypted message into the encrypted message box and enter the password used to encrypt the message and click decrypt, the decrypted message will be displayed in the decrypted message box.";
-var encryptedFileHelpMsg = "Click choose file and select a file to encrypt or decrypt.\nNext enter a key and click encrypt";
-var decryptedMsgHelp = "Select an encrypted file, enter the key and click decrypt. A download will begin for the deciphered file";
+var msgEncrypHelp = "Type a message into the message box and enter a password and click encrypt, the generated encrypted message will be displayed in the encrypted message box that will appear.<br><br>";
+var msgDecryptHelp = "Paste an encrypted message hash into the encrypted message box and enter the password used to encrypt the message and click decrypt, the decrypted message will be displayed in the decrypted message box that will appear.<br><br>";
+var encryptedFileHelpMsg = "Click choose file and select the file from your system you want to encrypt. Next enter a key and click encrypt.<br><br>If the file is < 5mb it will be stored in the keys tab and available for download. If the file is > 5mb it will be downloaded to your system." +
+  "<br><br>Your encrypted file will have a .encrypted file type";
+var decryptedMsgHelp = "Choose an encrypted file from your system, enter the key and click decrypt. A download will begin for the file if the key is correct.";
+var otherDecryptMessage = "This is meant to show visually how encryption works. Encryption completely hides the original item. For more practical use of encrypting messages, save text to a file and encrypt the file. This will allow you to decrypt the file later and view the original text.";
 var modalActive = false;
 
 
@@ -12,11 +14,13 @@ $('#helpButton').click(function ()
     modalActive = true;
     $('#modal-container').removeAttr('class').addClass('one');
     $('body').addClass('modal-active');
-    $('<h2>').text("File Encryption:").appendTo('#modalContent');
-    $('<p>').text(encryptedFileHelpMsg).appendTo('#modalContent').attr('id', 'marginMessage');
+    $('<h2>').html("File Encryption:").appendTo('#modalContent');
+    $('<p>').html(encryptedFileHelpMsg).appendTo('#modalContent').attr('id', 'marginMessage');
 
-    $('<h2>').text("File Decryption:").appendTo('#modalContent');
-    $('<p>').text(decryptedMsgHelp).appendTo('#modalContent')
+    $('<h2>').html("File Decryption:").appendTo('#modalContent');
+    $('<p>').html(decryptedMsgHelp).appendTo('#modalContent')
+
+    createBlur();
   }
 })
 
@@ -26,11 +30,14 @@ $('#helpButtonMsg').click(function () {
     modalActive = true;
     $('#modal-container').removeAttr('class').addClass('one');
     $('body').addClass('modal-active');
-    $('<h2>').text("Message Encryption:").appendTo('#modalContent');
-    $('<p>').text(msgEncrypHelp).appendTo('#modalContent').addClass('mb-4')
+    $('<h2>').html("Message Encryption:").appendTo('#modalContent');
+    $('<p>').html(msgEncrypHelp).appendTo('#modalContent')
+    $('<h2>').html("Message Decryption:").appendTo('#modalContent');
+    $('<p>').html(msgDecryptHelp).appendTo('#modalContent');
+    $('<h2>').html("Message Info:").appendTo('#modalContent');
+    $('<p>').html(otherDecryptMessage).appendTo('#modalContent');
 
-    $('<h2>').text("Message Decryption:").appendTo('#modalContent');
-    $('<p>').text(msgDecryptHelp).appendTo('#modalContent');
+    createBlur();
   }
 })
 
@@ -38,6 +45,14 @@ $('#modal-container').click(function ()
 {
   $(this).addClass('out');
   $('body').removeClass('modal-active');
+  $('#modalBlur').removeClass('active');
+  $('#modalBlur').remove();
   $('#modalContent').empty();
   modalActive = false;
 });
+
+function createBlur(){
+  var blurDiv = $('<div>').attr('id', 'modalBlur');
+  blurDiv.appendTo('#main');
+  blurDiv.addClass('active');
+}
